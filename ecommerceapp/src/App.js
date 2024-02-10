ff1d3182a0dd9c16322262e45ea046a03bb117bd
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-02-10 13:26:13 -0300

    adicionando o header

--- ecommerceapp/src/App.js
+++ ecommerceapp/src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Header from './components/header/header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
       <Routes>
         <Route path="/" element={<Header/>}></Route>
--- ecommerceapp/src/components/header/header.js
+++ ecommerceapp/src/components/header/header.js
@@ -1,21 +1,27 @@
 import React from "react";
 import "./header.css"
 
+import Cart from "../../assets/icons/cart.png";
+
 function Header(){
     return(
         <header>
-            <div className="headerlogo">
-                <h1>eCommerce</h1>
+            <div className="headerarea">
+                <div className="headerlogo">
+                    <h1>eCommerce</h1>
+                </div>
+                <div className="headerinput">
+                    <input type="text" placeholder="Procure um produto"></input>
+                </div>
+                <div className="headerlogin">
+                    <p>Login</p>
+                </div>
+                <div className="header-cart">
+                    <img src={Cart} alt="cart"></img>
+                    <p>Cart</p>
+                </div>
             </div>
-            <div className="headerinput">
-                <input type="text" placeholder="Procure um produto"></input>
-            </div>
-            <div className="headerlogin">
-                <p>Login</p>
-            </div>
-            <div className="header-cart">
-                
-            </div>
+            
         </header>
     )
 }
