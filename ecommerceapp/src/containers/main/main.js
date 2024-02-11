9213d59b6748d53a8f67bc101ae6bff42cffe2c1
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-02-10 21:32:39 -0300

    adicionado main

---
+++ ecommerceapp/src/containers/main/main.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+import "./main.css";
+
+
+
+function Main(){
+    return(
+        <main>
+            <section className="banner">
+                
+            </section>
+        </main>
+    )
+}
