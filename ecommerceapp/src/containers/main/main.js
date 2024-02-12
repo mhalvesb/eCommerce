bfcf0e96edf00b14f986e986aa2a307d83f7bb23
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-02-12 13:41:14 -0300

    atualizado

--- ecommerceapp/src/containers/main/main.js
+++ ecommerceapp/src/containers/main/main.js
@@ -16,6 +16,11 @@ function Main(){
                     <h1></h1>
                 </div>
             </section>
+            <div className="mainbar">
+                <div className="ic1">Entrega rápida para todo o Brasil</div>
+                <div className="ic1">Ganhe ofertas e descontos nos produtos</div>
+                <div className="ic1"></div>
+            </div>
             </main>
         </div>
         
