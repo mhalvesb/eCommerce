fa7af874eb69b1d078194faa31d1bb6ce4832cfd
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-02-19 20:29:52 -0300

    atualizado

--- ecommerceapp/src/containers/blusas/blusas.js
+++ ecommerceapp/src/containers/blusas/blusas.js
@@ -8,6 +8,11 @@ import Footer from "../../components/footer/footer.js";
 
 
 import Blusa from "../../assets/images/blusabranca.avif";
+import Blusapreta from "../../assets/images/camisapreta.png";
+import Blusaturquesa from "../../assets/images/floralturquesa.jpg";
+import Azulpreto from "../../assets/images/gradienteazulpreto.png";
+import Pretovermelho from "../../assets/images/gradientepretovermelho.png";
+
 
 function Blusas(){
     return(
@@ -23,7 +28,42 @@ function Blusas(){
 
                             <div className="blusasproduct">
                                 <img src={Blusa} alt="blusa"></img>
-                                <h4>Blusa branca</h4>
+                                <h4>Blusa branca masculina</h4>
+                                <p>R$ 180,00</p>
+                                <button>Comprar</button>
+                            </div>
+
+                            <div className="blusasproduct">
+                                <img src={Blusapreta} alt="blusa"></img>
+                                <h4>Blusa preta masculina</h4>
+                                <p>R$ 150,00</p>
+                                <button>Comprar</button>
+                            </div>
+
+                            <div className="blusasproduct">
+                                <img src={Blusaturquesa} alt="blusa"></img>
+                                <h4>Blusa flor turquesa masculina</h4>
+                                <p>R$ 240,00</p>
+                                <button>Comprar</button>
+                            </div>
+
+                            <div className="blusasproduct">
+                                <img src={Azulpreto} alt="blusa"></img>
+                                <h4>Blusa gradiente azul masculina</h4>
+                                <p>R$ 300,00</p>
+                                <button>Comprar</button>
+                            </div>
+
+                            <div className="blusasproduct">
+                                <img src={Pretovermelho} alt="blusa"></img>
+                                <h4>Blusa gradiente masculina</h4>
+                                <p>R$ 300,00</p>
+                                <button>Comprar</button>
+                            </div>
+
+                            <div className="blusasproduct">
+                                <img src={Blusa} alt="blusa"></img>
+                                <h4>Blusa branca masculina</h4>
                                 <p>R$ 180,00</p>
                                 <button>Comprar</button>
                             </div>
