a5606496fe5e220a54ac96a1b7406a1fe2aaa32e
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-02-24 12:29:32 -0300

    adicionado carrinho

---
+++ ecommerceapp/src/components/cart/cart.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+import Blusa from "../../assets/images/blucabranca.avif";
+
+
+function Cart(){
+    return(
+        <div className="cart">
+            <div className="cartarea">
+                <ul>
+                    <li>
+                        <img src={Blusa} alt="item"></img>
+                        <p className="itemname">Camisa branca</p>
+                        <p className="itemprice">R$ 90,00</p>
+                        <div className="itemcount">
+                            <button>-</button>
+                            <p className="itemnumber">1</p>
+                            <button>+</button>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
