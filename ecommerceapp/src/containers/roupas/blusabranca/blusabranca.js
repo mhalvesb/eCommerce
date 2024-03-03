2d4218645bfc55b606cce3c5b12f1f0ff4e63f5a
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-03-03 12:25:54 -0300

    atualizando

--- ecommerceapp/src/containers/roupas/blusabranca/blusabranca.js
+++ ecommerceapp/src/containers/roupas/blusabranca/blusabranca.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+import "./blusabranca.css";
 
 import Header from "../../../components/header/header.js";
 import Footer from "../../../components/footer/footer.js";
@@ -16,20 +17,31 @@ function BlusaBranca(){
             <section>
             <div className="roupa">
                 <div className="roupaarea">
-                    <div className="roupaimg">
-                        <img src={Blusa} alt="roupa"></img>
-                    </div>
-                    <div className="roupatext">
-                        <h1>Blusa branca masculina</h1>
-                        <hr></hr>
-                        <p>R$ 200,00</p>
-                        <p>Cor: Branca</p>
+                    <div className="firstrow">
+                        <div className="roupaimg">
+                            <img src={Blusa} alt="roupa"></img>
+                        </div>
+                        <div className="roupatext">
+                            <h1>Blusa branca masculina</h1>
+                            <hr></hr>
+                            <p>R$ 200,00</p>
 
-                        <button className="addCart">Adicionar ao carrinho</button>
-                        <label>Consultar o frete</label>
-                        <input type="text" placeholder="Digite seu cep"></input> 
-                        <button>OK</button>
+                            <div className="color"><p className="grey">Cor: </p> <p>Branca</p> 
+                            <div className="colorradius">
+                                <div className="colorarea"></div>
+                            </div>
+                            </div>
+
+                            <button className="addCart">Adicionar ao carrinho</button>
+                            <label>Consultar o frete</label>
+                            <input type="text" placeholder="Digite seu cep"></input> 
+                            <button>OK</button>
+                        </div>
                     </div>
+                    <div className="secrow">
+                        <p>Descrição</p>
+                    </div>
+                    
                 </div>
             </div>
             </section>
