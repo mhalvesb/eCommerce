82ba511552684212dae52dd8b6b0c42a4231c63d
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-03-12 20:44:14 -0300

    criada area de login

--- ecommerceapp/src/App.js
+++ ecommerceapp/src/App.js
@@ -3,9 +3,14 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Main from "./containers/main/main.js";
 import Blusas from "./containers/blusas/blusas.js";
 import Calcas from "./containers/calcas/calcas.js";
+import Login from "./containers/login/login.js";
+
 import BlusaBranca from "./containers/roupas/blusabranca/blusabranca.js";
 import BlusaPreta from "./containers/roupas/blusapreta/blusapreta.js";
 import CalcaPreta from "./containers/roupas/calcapreta/calcapreta.js";
+
+
+
 import "./index.css";
 
 function App() {
@@ -16,6 +21,7 @@ function App() {
         <Route path="/" element={<Main/>}></Route>
         <Route path="/blusas" element={<Blusas/>}></Route>
         <Route path="/calcas" element={<Calcas/>}></Route>
+        <Route path="/login" element={<Login/>}></Route>
         <Route path="/roupas/blusabranca" element={<BlusaBranca/>}></Route>
         <Route path="/roupas/blusapreta" element={<BlusaPreta/>}></Route>
         <Route path="/roupas/calcapreta" element={<CalcaPreta/>}></Route>
---
+++ ecommerceapp/src/containers/login/login.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "./login.css";
+
+
+
+function Login(){
+    return(
+        <div className="login-area">
+            <h1>Ecommerce</h1>
+            <h6>Bom ver você novamente</h6>
+            <div className="login-container">
+                <form action="" method="post">
+                    
+                </form>
+            </div>
+        </div>
+    )
+}
+
+
+
+export default Login;
