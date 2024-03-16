3751e13e5c374c41823d07afaafc5048140c25e8
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-03-16 13:59:28 -0300

    atualizado

--- ecommerceapp/src/containers/singup/singup.js
+++ ecommerceapp/src/containers/singup/singup.js
@@ -12,30 +12,33 @@ function Singup(){
         return(
             <div className="singup-area">
                 <div className="sig-1">
+                    <div className="singup-container">
                     <h2>Ecommerce</h2>
-                    <h1>Boas vindas</h1>
-                </div>
-                <form>
-                    <div className="inputarea">
-                        <input type="email" name="emails" id="emails" required></input>
-                        <p>Seu e-mail</p>
-                    </div>
-                    <div className="inputarea">
-                            <input type="password" name="pass" id="pass" required></input>
-                            <p>Sua senha</p>
-                        </div>
-                        <button>Cadastrar-se</button>
+                        <h1>Boas vindas</h1>
+                        <form>
+                            <div className="inputarea">
+                                <input type="email" name="emails" id="emails" required></input>
+                                <p>Seu e-mail</p>
+                            </div>
+                            <div className="inputarea">
+                                    <input type="password" name="pass" id="pass" required></input>
+                                    <p>Sua senha</p>
+                                </div>
+                                <button>Cadastrar-se</button>
 
-                        <div className="login-nav">
-                            <Link to="/login">Já possui uma conta?</Link>
-                        </div>
-                </form>
+                                <div className="login-nav">
+                                    <Link to="/login">Já possui uma conta?</Link>
+                                </div>
+                        </form>
+                    </div>
+                </div>
+                
                 <div className="sig-2">
                     <h2>A melhor plataforma de roupas</h2>
                     <br></br>
                     <br></br>
 
-                    <h1>Ficamos felizes em  <br></br>te ver  novamente</h1>
+                    <h1>Ficamos felizes em  <br></br>ver você por aqui</h1>
                     <br></br>
                     <br></br>
                     <h1>Faça seu cadastro e obtenha das <br></br>melhores promoções de roupas</h1>
