cdd012b74f236a225f724f7793c7df5ca14265e1
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-03-16 13:37:52 -0300

    atualizando a area de cadastro

--- ecommerceapp/src/containers/singup/singup.js
+++ ecommerceapp/src/containers/singup/singup.js
@@ -11,7 +11,35 @@ function Singup(){
     
         return(
             <div className="singup-area">
-                
+                <div className="sig-1">
+                    <h2>Ecommerce</h2>
+                    <h1>Boas vindas</h1>
+                </div>
+                <form>
+                    <div className="inputarea">
+                        <input type="email" name="emails" id="emails" required></input>
+                        <p>Seu e-mail</p>
+                    </div>
+                    <div className="inputarea">
+                            <input type="password" name="pass" id="pass" required></input>
+                            <p>Sua senha</p>
+                        </div>
+                        <button>Cadastrar-se</button>
+
+                        <div className="login-nav">
+                            <Link to="/login">Já possui uma conta?</Link>
+                        </div>
+                </form>
+                <div className="sig-2">
+                    <h2>A melhor plataforma de roupas</h2>
+                    <br></br>
+                    <br></br>
+
+                    <h1>Ficamos felizes em  <br></br>te ver  novamente</h1>
+                    <br></br>
+                    <br></br>
+                    <h1>Faça seu cadastro e obtenha das <br></br>melhores promoções de roupas</h1>
+                </div>
             </div>
         )
 }
