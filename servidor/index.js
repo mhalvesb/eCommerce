bb61aab6e19cea0b6f8a8597b331ceca568d95c3
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-03-18 22:25:39 -0300

    adicionado o back end

---
+++ servidor/index.js
@@ -0,0 +1,24 @@
+const express= require("express");
+const app = express();
+
+
+
+
+
+app.get("/", (req, res) =>{
+    res.send("ok");
+})
+
+
+
+
+app.listen(8080, ()=>{
+    console.log("Servidor iniciado na porta 8080");
+})
+
+
+
+
+
+
+
