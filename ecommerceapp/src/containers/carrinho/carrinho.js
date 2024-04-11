e1cf4a310a0f2958e5118b1732adbeedc50de5aa
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-04-11 18:59:12 -0300

    atualizado

--- ecommerceapp/src/containers/carrinho/carrinho.js
+++ ecommerceapp/src/containers/carrinho/carrinho.js
@@ -1,6 +1,9 @@
 import React from "react";
 
 
+
+import Blusa from "../../assets/images/blusabranca.avif"
+
 import "./carrinho.css";
 
 
@@ -9,7 +12,20 @@ function Carrinho(){
     return(
         <div className="carrinho-area">
             <div className="side1">
-
+                
+                <div className="item">
+                    <img src={Blusa} alt="image"></img>
+                    <div className="item-options">
+                        <h4>Blusa branca masculina</h4>
+                        <div className="items-qtd">
+                            <p>Quantidade</p> <button>-</button> <p>1</p> <button>+</button>
+                        </div>
+                    </div>
+                    <div className="more">
+                        <img src="" alt="image"></img>
+                        <p>199</p>
+                    </div>
+                </div>
             </div>
             <div className="side2">
 
