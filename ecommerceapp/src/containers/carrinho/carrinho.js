2622270ede9edcbd95ff39d2a103aacb8a9ba7ca
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-04-11 18:46:18 -0300

    adicionando css

--- ecommerceapp/src/containers/carrinho/carrinho.js
+++ ecommerceapp/src/containers/carrinho/carrinho.js
@@ -1,12 +1,19 @@
 import React from "react";
 
 
+import "./carrinho.css";
+
 
 
 function Carrinho(){
     return(
-        <div>
-            
+        <div className="carrinho-area">
+            <div className="side1">
+
+            </div>
+            <div className="side2">
+
+            </div>
         </div>
     )
 }
