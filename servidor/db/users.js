c21a661f7c0ebc44e319ad378a1c76fd6e2fb18a
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-05-09 02:41:41 -0300

    atualizado

--- servidor/db/users.js
+++ servidor/db/users.js
@@ -1,8 +1,13 @@
 const Sequelize = require("sequelize");
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD,{
     host: process.env.DATABASE_HOST,
-    dialect: "mysql",
     port: process.env.DATABASE_PORT,
+    dialect: "mysql",
+    dialectOptions: {
+        ssl: {
+            ca: process.env.DATABASE_CA
+        }
+    }
 });
 
 
