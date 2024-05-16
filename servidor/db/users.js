18868c0817141e0ded2fa7ca620976bef6b0ffa7
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-05-16 00:44:21 -0300

    atualizando banco de dados

--- servidor/db/users.js
+++ servidor/db/users.js
@@ -1,7 +1,7 @@
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize("sql10705200", "sql10705200", "e8waRMlr8R",{
-    host: "sql10.freesqldatabase.com",
-    port: 3306,
+const sequelize = new Sequelize(process.env.DB_USER, process.env.DB_USER, process.env.DB_PASS,{
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     dialectModule: require('mysql2'),
     
