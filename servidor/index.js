130c9d5e35c860445825643f466cf33798be473c
Matheus Rodrigues <matheus.alvesbr21@gmail.com>
2024-07-10 13:11:53 -0300

    adicionando post de login

--- servidor/index.js
+++ servidor/index.js
@@ -3,7 +3,15 @@ const app = express();
 const bodyParser = require("body-parser");
 const {MongoClient, ServerApiVersion } = require("mongodb");
 const myUrl = "mongodb+srv://matheusalvesbr21:mth123@cluster0.ik1mw9r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const session = require("express-session");
 
+app.use(session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoClientOptions = {
   useNewUrlParser: true,
@@ -25,6 +33,10 @@ async function connectMyMongoDb(){
     try{
         await client.connect();
         console.log("Cliente conectado");
+        const db = client.db("usuarios");
+        const collection = db.collection("meuusuarios");
+        const result = await collection.find({}).toArray();
+        console.log(result);
     } catch(err){
         console.log(err);
     }
@@ -32,6 +44,21 @@ async function connectMyMongoDb(){
 
 connectMyMongoDb();
 
+
+
+app.post("/login", async (req, res ,next) =>{
+    try{
+        console.log(req.body);
+        await client.connect();
+        const db = client.db("usuarios");
+        const collection = db.collection("meuusuarios");
+        const login = await collection.insertOne({usuarios: req.body.usuario, senha: req.body.password});
+        res.status(200).send("Login efetuado com sucesso");
+        next();
+    }catch(err){
+        res.status(500).send("Algum erro aconteceu");
+    }
+})
 
 
 
